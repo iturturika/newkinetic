3ea270ec0d87cea0b5267a33a74a1df0e5383b16
ozzy <iturturika89@gmail.com>
2024-02-14 17:29:41 +0200

    new page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,10 +1,15 @@
-
+'use client'
 import React from 'react'
+import Header from "@/components/Header/Header";
+import Cart from "@/components/Cart/Cart";
+import './page.scss'
 
 const page = () => {
+  const [isOpenCart, setIsOpenCart] = React.useState<any>(false);
   return (
-    <div>
-
+    <div className='product-page'>
+      <Header setIsOpenCart={setIsOpenCart}/>
+      <Cart isOpenCart={isOpenCart} setIsOpenCart={setIsOpenCart}/>
     </div>
   )
 }
